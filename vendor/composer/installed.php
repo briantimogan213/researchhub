43ab67eb90e62c3dfc47c8489bf0a44dc2dd2013
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-01-10 08:08:57 +0800

    vendor added. waiting for payment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'smcc/research-hub',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'def5bf0d385314a4991a28961fbc4ce14801e9b4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'smcc/research-hub' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'def5bf0d385314a4991a28961fbc4ce14801e9b4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
