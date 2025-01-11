8a76b207d6b5ba6f3ea592105ec1e83805ffe03e
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-01-11 14:43:32 +0800

    ok homepage

--- src/Views/Pages/HomePage.php
+++ src/Views/Pages/HomePage.php
@@ -16,11 +16,6 @@ class HomePage extends View
       $this->getTitle(),
       function () {
 ?>
-  <div class="relative min-h-[150px]">
-    <div class="flex flex-col justify-center items-center my-10 gap-y-8" id="home-announcement-container">
-      <div class="w-16 h-16 border-4 border-t-4 border-gray-300 border-t-blue-500 border-solid rounded-full animate-spin"></div>
-    </div>
-  </div>
   <div class="relative">
     <aside class="relative mx-auto mb-3 lg:mx-0 lg:absolute right-2 top-4 min-w-[400px] max-w-[500px] p-3" id="most-view-container-theses"></aside>
     <aside class="relative mx-auto mb-3 lg:mx-0 lg:absolute left-2 top-4 min-w-[400px] max-w-[500px] p-3" id="most-view-container-journals"></aside>
@@ -29,13 +24,14 @@ class HomePage extends View
         <h1>Where Knowledge Meets</h1>
         <h1>Innovation</h1>
       </div>
-      <div class="mx-auto flex flex-col justify-center items-center my-10">
-        <a href="<?= Router::getPathname("/thesis") ?>">
-          <button type="button" class="mx-auto hover:drop-shadow-lg hover:scale-110 font-[Poppins] font-[400] text-center text-[22px] leading-[33px] w-[220px] h-[48px] bg-[#2487CE] rounded-[5px] text-white">
-              Explore
-          </button>
-        </a>
-      </div>
+    </div>
+  </div>
+  <div class="mt-2">
+    &nbsp;
+  </div>
+  <div class="relative min-h-[150px]">
+    <div class="flex flex-col justify-center items-center my-10 gap-y-8" id="home-announcement-container">
+      <div class="w-16 h-16 border-4 border-t-4 border-gray-300 border-t-blue-500 border-solid rounded-full animate-spin"></div>
     </div>
   </div>
   <div class="mt-[200px]">
--- src/Views/react/src/home.tsx
+++ src/Views/react/src/home.tsx
@@ -1,6 +1,6 @@
 import Modal from './global/modal';
 import PdfViewer from './global/pdfviewer';
-import { React, ReactDOM, ReactPlayerYoutube, parseMarkup, pathname } from './imports';
+import { React, ReactDOM, ReactPlayerYoutube, clsx, parseMarkup, pathname } from './imports';
 
 function VideoPlayer({ url }: { url: string }) {
   return <ReactPlayerYoutube url={url} width="100%" height="100%" controls />
@@ -45,7 +45,7 @@ function Announcements() {
   {announcements.map((announcement: {a_type: "video"|"text", url?: string, message?: string, title: string, expires: string, id?: string|number}, i: number) => (
     <React.Fragment key={"announcement_" + i + announcement?.id}>
       {announcement.a_type === "video" && !checkExpired(announcement.expires) && (
-        <div className="w-[500px] md:w-[700px] lg:w-[1000px] min-w-[500px] bg-gray-100 border-l-2 border-blue-500 rounded">
+        <div className="w-[500px] md:w-[700px] lg:w-[1000px] min-w-[500px] bg-gray-100 rounded">
           <div className="text-xl py-3 px-4 border-b  text-blue-500 font-semibold"><h2>{announcement.title}</h2></div>
           <div className="w-full h-full px-[10%] py-[5%] aspect-video">
             <VideoPlayer url={announcement.url || ""} />
@@ -53,7 +53,7 @@ function Announcements() {
         </div>
       )}
       {announcement.a_type === "text" && !checkExpired(announcement.expires) && (
-        <div className="w-[500px] md:w-[700px] lg:w-[1000px] min-w-[500px] bg-gray-100 border-l-2 border-blue-500 rounded rich-text-editor">
+        <div className="w-[500px] md:w-[700px] lg:w-[1000px] min-w-[500px] bg-gray-100 rounded rich-text-editor">
           <div className="text-xl py-3 px-4  border-b text-blue-500 font-semibold"><h2>{announcement.title}</h2></div>
           <div className="text-center p-3 text-slate-900 my-3 announcement editor-area">
             {parseMarkup(announcement.message!)?.split("\n").map((v: any) => <><span dangerouslySetInnerHTML={{ __html: v }} /><br /></> || "")}
@@ -87,6 +87,7 @@ function MostViewsTheses() {
   const [pdfUrl, setPdfUrl] = React.useState<string|undefined>()
   const [pdfTitle, setPdfTitle] = React.useState<string|undefined>()
   const [pdfAuthor, setPdfAuthor] = React.useState<string|undefined>()
+  const [openView, setOpenView] = React.useState<boolean>(false)
 
   const onViewPdf = React.useCallback((uri: string, title: string, author: string) => {
     setPdfTitle(title);
@@ -99,27 +100,40 @@ function MostViewsTheses() {
     onViewPdf && onViewPdf(uri, title, author + ' (' + year + ')')
   }, [onViewPdf]);
 
+
   const authenticated = React.useMemo(() => {
     return window?.sessionStorage.getItem('authenticated') == 'true';
   }, []);
+
   return (<>
-    <div className="bg-slate-100 p-3 mb-4">
+    <div
+      className={clsx(
+        "bg-slate-50 p-3 mb-4 border shadow relative overflow-hidden transition-all delay-0 duration-500",
+        openView ? "" : "h-[50px]"
+      )}
+    >
       <h1 className="font-bold text-lg">Most Viewed Thesis</h1>
-      <div className="grid grid-cols-1">
-        {isLoading && (
-          <div className="w-full mt-4"><div className="mx-auto w-8 h-8 border-4 border-t-transparent border-blue-500 border-solid rounded-full animate-spin"></div></div>
-        )}
-        {!isLoading && data.length === 0 && (
-          <div className="w-full mt-4"><div className="mx-auto text-center text-slate-600">No thesis has been viewed yet.</div></div>
-        )}
-        {!isLoading && data.length > 0 && (
-          data.map((thesis: { title: string, views: number, id: number|string }, i: number) => (
-            <div onClick={() => handleView(thesis as any)} key={"thesis_" + i + "_" + thesis.id} className="py-4 px-2 cursor-pointer hover:*:text-blue-500 border-gray-200 flex flex-nowrap justify-between items-center">
-              <div className="font-semibold text-slate-900">{i + 1}. {thesis.title}</div>
-              <div className="text-slate-600 italic text-xs"><span className="material-symbols-outlined text-xs">visibility</span>&nbsp;{thesis.views}</div>
-            </div>
-          ))
-        )}
+      <button type="button" className="absolute right-2 top-2 hover:text-gray-600" onClick={() => setOpenView(prev => !prev)}>
+        {!openView && <span className="material-symbols-outlined">menu</span>}
+        {openView && <span className="material-symbols-outlined">close</span>}
+      </button>
+      <div className="max-h-[180px] overflow-y-auto">
+        <div className="grid grid-cols-1">
+          {isLoading && (
+            <div className="w-full mt-4"><div className="mx-auto w-8 h-8 border-4 border-t-transparent border-blue-500 border-solid rounded-full animate-spin"></div></div>
+          )}
+          {!isLoading && data.length === 0 && (
+            <div className="w-full mt-4"><div className="mx-auto text-center text-slate-600">No thesis has been viewed yet.</div></div>
+          )}
+          {!isLoading && data.length > 0 && (
+            data.map((thesis: { title: string, views: number, id: number|string }, i: number) => (
+              <div onClick={() => handleView(thesis as any)} key={"thesis_" + i + "_" + thesis.id} className="py-4 px-2 cursor-pointer *:hover:text-blue-500 border-b border-gray-200 flex flex-nowrap justify-between items-center">
+                <div className="font-[400] text-slate-900">{thesis.title}</div>
+                <div className="text-slate-900 text-md pr-4"><span className="material-symbols-outlined text-sm translate-y-0.5">visibility</span>&nbsp;{thesis.views}</div>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
     <Modal open={!!pdfUrl} onClose={() => { setPdfUrl(undefined); setPdfTitle(undefined); setPdfAuthor(undefined); }} content={!!authenticated ? <PdfViewer src={pdfUrl} /> : <div className="w-full text-center min-h-[150px] pt-16">Please <a href={pathname("/login")} className="text-sky-700 underline">login</a> to view journal.</div>} header={pdfTitle} showCancelButton={false} showConfirmButton={false} footer={pdfAuthor} />
