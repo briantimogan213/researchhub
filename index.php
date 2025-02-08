54276d6a2554403e91b52c396dfa3f070b554565
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-02-08 10:43:01 +0800

    handle fatal error exception

--- index.php
+++ index.php
@@ -7,10 +7,23 @@ define('APP_PATH', realpath(__DIR__ . '/.'));
 
 require_once implode(DIRECTORY_SEPARATOR, [APP_PATH, 'vendor', 'autoload.php']);
 
+function handleFatalError()
+{
+    $error = error_get_last();
+    if ($error !== null) {
+      $controller = new \Smcc\ResearchHub\Controllers\ViewController();
+      $view = $controller->error("Fatal Error - " . str_replace("\n", "<br />", $error['message']) . " <br />File: {$error['file']} <br />Line: {$error['line']}");
+      $view->render();
+      exit;
+    }
+}
+
+register_shutdown_function('handleFatalError');
+
 set_error_handler(function($severity, $message, $file, $line) {
   if ($severity === E_ERROR || $severity === E_USER_ERROR) {
     $controller = new \Smcc\ResearchHub\Controllers\ViewController();
-    $view = $controller->error("Internal Server Error - " . str_replace("\n", "<br />", $message) . " | File: $file | Line: $line |", debug_backtrace());
+    $view = $controller->error("Internal Server Error - " . str_replace("\n", "<br />", $message) . " <br />File: $file <br />Line: $line <br />", debug_backtrace());
     $view->render();
     exit;
   }
